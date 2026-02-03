dd058806d1f8c4cb425e85eacd98f4b6fe243d76
patel <neelp060@gmail.com>
2026-02-02 19:09:18 -0500

    Added 2 more cpomponents for todoCard and todoList

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import ToDoInput from './components/ToDoInput.jsx'
+import ToDoList from './components/ToDoList.jsx'
 
 function App() {
 
   return (
-    <>
-     
-    </>
+    <main>
+      <ToDoInput />
+      <ToDoList />
+    </main>
   )
 }
 
